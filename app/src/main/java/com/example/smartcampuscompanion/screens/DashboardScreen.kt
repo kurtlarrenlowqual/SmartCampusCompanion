fb6fa1a3d0a5fb055c23bde95e1954e10d224724
Joemrich <joemrichsalcedo@gmail.com>
2026-02-01 19:36:26 +0800

    Added top bar and hamburger menu

--- app/src/main/java/com/example/smartcampuscompanion/screens/DashboardScreen.kt
+++ app/src/main/java/com/example/smartcampuscompanion/screens/DashboardScreen.kt
@@ -15,15 +15,52 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.navigation.NavHostController
 import com.example.smartcampuscompanion.util.SessionManager
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DashboardScreen(navController: NavController, context: Context) {
     val username = SessionManager.getUsername(context)
 
-    Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
-    Text (text = "Welcome, $username!", style = MaterialTheme.typography.headlineMedium)
+    // Use of scaffold and TopAppBar for navigation.
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Dashboard") },
+                navigationIcon = {
+                    IconButton(onClick = {
+                    }) {
+                        Icon(
+                            imageVector = Icons.Default.Menu,
+                            contentDescription = "Menu"
+                        )
+                    }
+                }
+            )
+        },
+        content = { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = "Welcome, $username!",
+                    style = MaterialTheme.typography.headlineMedium
+                )
 
-    Spacer(modifier = Modifier.height(24.dp))
-    }
+                Spacer(modifier = Modifier.height(24.dp))
+
+
+            }
+        }
+    )
 }
