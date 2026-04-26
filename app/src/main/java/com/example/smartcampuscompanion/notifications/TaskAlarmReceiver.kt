60e9ecd393b905c38f1b51a04a5b9a24c2fafde9
Kurt Larren Rosales <kurtlarrenr@gmail.com>
2026-04-27 02:04:12 +0800

    Update TaskAlarmReceiver with minor code formatting
    Added a newline for better code readability in TaskAlarmReceiver.kt.

--- app/src/main/java/com/example/smartcampuscompanion/notifications/TaskAlarmReceiver.kt
+++ app/src/main/java/com/example/smartcampuscompanion/notifications/TaskAlarmReceiver.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 
 class TaskAlarmReceiver : BroadcastReceiver() {
+
     override fun onReceive(context: Context, intent: Intent) {
         val title  = intent.getStringExtra("task_title") ?: "Task"
         val notifId = intent.getIntExtra("notif_id", 3000)
